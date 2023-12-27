53719b42b633b62b20f702fd16b160517f8b3939
artemis_keg <artemiskegerogloy@gmail.com>
2023-12-27 18:48:39 +0200

    final board, players, status, get board/piece

--- lib/ludo.php
+++ lib/ludo.php
@@ -2,61 +2,80 @@
 require_once "../lib/db_connect.php"; 
 require_once "../lib/board.php";
 require_once "../lib/game.php";
+require_once "../lib/users.php";
 
 $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 $input = json_decode(file_get_contents('php://input'),true);
+if($input==null) {
+    $input=[];
+}
+if(isset($_SERVER['HTTP_X_TOKEN'])) {
+    $input['token']=$_SERVER['HTTP_X_TOKEN'];
+} else {
+    $input['token']='';
+}
 
-# print_r($request );
-
- switch ($r=array_shift($request)) {
+switch ($r=array_shift($request)) {
     case 'board' : 
-	switch ($b=array_shift($request)) {
-		case '': 
-		case null: handle_board($method);break;
-		case 'piece': handle_piece($method, $request[0],$request[1],$input);
-					break;
-//		case 'player': handle_player($method, $request[0],$input);
-//					break;
-		default: header("HTTP/1.1 404 Not Found");
-				break;
-	}
-		break;
-	case 'status': 
+        switch ($b=array_shift($request)) {
+            case '':
+            case null: handle_board($method,$input);
+                        break;
+            case 'piece': handle_piece($method, $request[0], $request[1], $request[2], $request[3], $input);
+                        break;
+            }
+            break;
+    case 'define_player' : define_player($method, $request[0],$request[1]);
+                break;
+    case 'players': handle_player($method, $request, $input);
+	        break;
+    case 'status': 
 		if(sizeof($request)==0) {handle_status($method);}
 		else {header("HTTP/1.1 404 Not Found");}
-		break;
-	case 'players': handle_player($method, $request,$input);
-			break;
-    default: 	
-	header("HTTP/1.1 404 Not Found");
-    print "<h1>not FOUND</h1>";
-	exit;
+			    break; 
+    case 'reset_color' : reset_color();
+            break;
+ 	default:  header("HTTP/1.1 404 Not Found");
+            exit;
 }
 
 
-function handle_board($method) {
+function handle_board($method,$input) {
     if($method=='GET') {
-            show_board();
+            show_board($input);
     } else if ($method=='POST') {
-           reset_board();
+           reset_board($input);
+           show_board($input);
     } else {
         header('HTTP/1.1 405 Method Not Allowed');
     }
     
 }
 
-function handle_piece($method, $x,$y,$input) {
+function handle_piece($method, $x, $y, $x2, $y2, $token) {
     if($method=='GET') {
         show_piece($x,$y);
-} else ($method=='POST') {
-        move_piece($x,$y,$input['x'],$input['y']);//,$input['token']);
+} else if ($method=='PUT') {
+    move_piece($x,$y,$x2,$y2,$token);
 }
 
 }
 
 function handle_player($method, $p,$input) {
-    ;
+    switch ($b=array_shift($p)) {
+        //	case '':
+        //	case null: if($method=='GET') {show_users($method);}
+        //			   else {header("HTTP/1.1 400 Bad Request"); 
+        //					 print json_encode(['errormesg'=>"Method $method not allowed here."]);}
+        //                break;
+            case 'R': 
+            case 'P': handle_user($method, $b, $input);
+                        break;
+            default: header("HTTP/1.1 404 Not Found");
+                     print json_encode(['errormesg'=>"Player $b not found."]);
+                     break;
+        }
 }
 
 function handle_status($method) {
@@ -66,11 +85,33 @@ function handle_status($method) {
         header('HTTP/1.1 405 Method Not Allowed');
     }
 }
-function do_move($x,$y,$x2,$y2) {
-	global $mysqli;
-	$sql = 'call `move_piece`(?,?,?,?);';
-	$st = $mysqli->prepare($sql);
-	$st->bind_param('iiii',$x,$y,$x2,$y2 );
-	$st->execute();
-}
+
+// function define_player($method, $username, $n, $length = 16) {
+//     if($method=='PUT'){
+//         if($n==0){
+//             $piece_color='R';
+//             $string = uniqid(rand());
+//             $randomString = substr($string, 0, $length);
+//             $username = $data['username'];
+//             global $mysqli;
+//             $sql = "INSERT INTO players VALUES ('$username', 'R', '$randomString')";
+// 	        $st = $mysqli->prepare($sql);
+// 	        $st->bind_param('iii', $username, $piece_color, $randomString);
+// 	        $st->execute();
+//         }
+//         else{
+//             $piece_color='P';
+//             $string = uniqid(rand());
+//             $randomString = substr($string, 0, $length);
+//             global $mysqli;
+// 	        $sql = "INSERT INTO players (username, piece_color, token) VALUES (?, ?, ?)";
+// 	        $st = $mysqli->prepare($sql);
+// 	        $st->bind_param('iii', $username, $piece_color, $randomString);
+// 	        $st->execute();
+//         }
+        
+//     } else {
+//         header('HTTP/1.1 405 Method Not Allowed');
+//     }
+// }
 ?>
