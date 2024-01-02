5e37818ea9ee90c548114e6b41b9fa1b73760f19
artemis_keg <artemiskegerogloy@gmail.com>
2024-01-02 20:22:44 +0200

    export database

--- schema.sql
+++ schema.sql
@@ -38,7 +38,7 @@ CREATE TABLE `board` (
 
 LOCK TABLES `board` WRITE;
 /*!40000 ALTER TABLE `board` DISABLE KEYS */;
-INSERT INTO `board` VALUES (1,1,'R',NULL,NULL),(1,2,'R',NULL,NULL),(1,3,'R',NULL,NULL),(1,4,'R',NULL,NULL),(1,5,'R',NULL,NULL),(1,6,'W','R','K2'),(1,7,'W',NULL,NULL),(1,8,'W',NULL,NULL),(1,9,'W',NULL,NULL),(1,10,'W',NULL,NULL),(1,11,'W',NULL,NULL),(1,12,'W',NULL,NULL),(1,13,'W',NULL,NULL),(2,1,'R',NULL,NULL),(2,2,'W',NULL,NULL),(2,3,'W',NULL,NULL),(2,4,'W',NULL,NULL),(2,5,'R',NULL,NULL),(2,6,'W',NULL,NULL),(2,7,'W',NULL,NULL),(2,8,'W',NULL,NULL),(2,9,'W',NULL,NULL),(2,10,'W',NULL,NULL),(2,11,'W',NULL,NULL),(2,12,'W',NULL,NULL),(2,13,'W',NULL,NULL),(3,1,'R',NULL,NULL),(3,2,'W',NULL,NULL),(3,3,'W',NULL,NULL),(3,4,'W',NULL,NULL),(3,5,'R',NULL,NULL),(3,6,'W',NULL,NULL),(3,7,'W',NULL,NULL),(3,8,'W',NULL,NULL),(3,9,'W',NULL,NULL),(3,10,'W',NULL,NULL),(3,11,'W',NULL,NULL),(3,12,'W',NULL,NULL),(3,13,'W',NULL,NULL),(4,1,'R',NULL,NULL),(4,2,'W',NULL,NULL),(4,3,'W',NULL,NULL),(4,4,'W',NULL,NULL),(4,5,'R',NULL,NULL),(4,6,'W',NULL,NULL),(4,7,'W',NULL,NULL),(4,8,'W',NULL,NULL),(4,9,'W',NULL,NULL),(4,10,'W',NULL,NULL),(4,11,'W',NULL,NULL),(4,12,'W',NULL,NULL),(4,13,'W',NULL,NULL),(5,1,'R',NULL,NULL),(5,2,'R',NULL,NULL),(5,3,'R',NULL,NULL),(5,4,'R',NULL,NULL),(5,5,'R',NULL,NULL),(5,6,'W',NULL,NULL),(5,7,'W',NULL,NULL),(5,8,'W',NULL,NULL),(5,9,'W',NULL,NULL),(5,10,'W',NULL,NULL),(5,11,'W',NULL,NULL),(5,12,'W',NULL,NULL),(5,13,'W',NULL,NULL),(6,1,'W',NULL,NULL),(6,2,'R',NULL,NULL),(6,3,'W',NULL,NULL),(6,4,'W',NULL,NULL),(6,5,'W',NULL,NULL),(6,6,'W',NULL,NULL),(6,7,'W',NULL,NULL),(6,8,'W',NULL,NULL),(6,9,'W',NULL,NULL),(6,10,'W',NULL,NULL),(6,11,'W',NULL,NULL),(6,12,'W',NULL,NULL),(6,13,'W',NULL,NULL),(7,1,'W',NULL,NULL),(7,2,'W',NULL,NULL),(7,3,'W',NULL,NULL),(7,4,'W',NULL,NULL),(7,5,'R','R','K1'),(7,6,'W',NULL,NULL),(7,7,'W',NULL,NULL),(7,8,'W',NULL,NULL),(7,9,'P',NULL,NULL),(7,10,'P',NULL,NULL),(7,11,'W','P','K1'),(7,12,'W',NULL,NULL),(7,13,'W',NULL,NULL),(8,1,'W',NULL,NULL),(8,2,'W',NULL,NULL),(8,3,'W',NULL,NULL),(8,4,'W',NULL,NULL),(8,5,'W',NULL,NULL),(8,6,'W',NULL,NULL),(8,7,'W',NULL,NULL),(8,8,'W',NULL,NULL),(8,9,'W',NULL,NULL),(8,10,'W',NULL,NULL),(8,11,'W',NULL,NULL),(8,12,'P','P','K2'),(8,13,'W',NULL,NULL),(9,1,'W',NULL,NULL),(9,2,'W',NULL,NULL),(9,3,'W',NULL,NULL),(9,4,'W',NULL,NULL),(9,5,'W',NULL,NULL),(9,6,'W',NULL,NULL),(9,7,'W',NULL,NULL),(9,8,'W',NULL,NULL),(9,9,'P',NULL,NULL),(9,10,'P',NULL,NULL),(9,11,'P',NULL,NULL),(9,12,'P',NULL,NULL),(9,13,'P',NULL,NULL),(10,1,'W',NULL,NULL),(10,2,'W',NULL,NULL),(10,3,'W',NULL,NULL),(10,4,'W',NULL,NULL),(10,5,'W',NULL,NULL),(10,6,'W',NULL,NULL),(10,7,'W',NULL,NULL),(10,8,'W',NULL,NULL),(10,9,'P',NULL,NULL),(10,10,'W',NULL,NULL),(10,11,'W',NULL,NULL),(10,12,'W',NULL,NULL),(10,13,'P',NULL,NULL),(11,1,'W',NULL,NULL),(11,2,'W',NULL,NULL),(11,3,'W',NULL,NULL),(11,4,'W',NULL,NULL),(11,5,'W',NULL,NULL),(11,6,'W',NULL,NULL),(11,7,'W',NULL,NULL),(11,8,'W',NULL,NULL),(11,9,'P',NULL,NULL),(11,10,'W',NULL,NULL),(11,11,'W',NULL,NULL),(11,12,'W',NULL,NULL),(11,13,'P',NULL,NULL),(12,1,'W',NULL,NULL),(12,2,'W',NULL,NULL),(12,3,'W',NULL,NULL),(12,4,'W',NULL,NULL),(12,5,'W',NULL,NULL),(12,6,'W',NULL,NULL),(12,7,'W',NULL,NULL),(12,8,'W',NULL,NULL),(12,9,'P',NULL,NULL),(12,10,'W',NULL,NULL),(12,11,'W',NULL,NULL),(12,12,'W',NULL,NULL),(12,13,'P',NULL,NULL),(13,1,'W',NULL,NULL),(13,2,'W',NULL,NULL),(13,3,'W',NULL,NULL),(13,4,'W',NULL,NULL),(13,5,'W',NULL,NULL),(13,6,'W',NULL,NULL),(13,7,'W',NULL,NULL),(13,8,'W',NULL,NULL),(13,9,'P',NULL,NULL),(13,10,'P',NULL,NULL),(13,11,'P',NULL,NULL),(13,12,'P',NULL,NULL),(13,13,'P',NULL,NULL);
+INSERT INTO `board` VALUES (1,1,'R',NULL,NULL),(1,2,'R',NULL,NULL),(1,3,'R',NULL,NULL),(1,4,'R',NULL,NULL),(1,5,'R',NULL,NULL),(1,6,'W',NULL,NULL),(1,7,'W',NULL,NULL),(1,8,'W',NULL,NULL),(1,9,'W',NULL,NULL),(1,10,'W',NULL,NULL),(1,11,'W',NULL,NULL),(1,12,'W',NULL,NULL),(1,13,'W',NULL,NULL),(2,1,'R',NULL,NULL),(2,2,'W',NULL,NULL),(2,3,'W',NULL,NULL),(2,4,'W',NULL,NULL),(2,5,'R',NULL,NULL),(2,6,'W',NULL,NULL),(2,7,'W',NULL,NULL),(2,8,'W',NULL,NULL),(2,9,'W',NULL,NULL),(2,10,'W',NULL,NULL),(2,11,'W',NULL,NULL),(2,12,'W',NULL,NULL),(2,13,'W',NULL,NULL),(3,1,'R',NULL,NULL),(3,2,'W','R','K2'),(3,3,'W',NULL,NULL),(3,4,'W',NULL,NULL),(3,5,'R',NULL,NULL),(3,6,'W',NULL,NULL),(3,7,'W',NULL,NULL),(3,8,'W',NULL,NULL),(3,9,'W',NULL,NULL),(3,10,'W',NULL,NULL),(3,11,'W',NULL,NULL),(3,12,'W',NULL,NULL),(3,13,'W',NULL,NULL),(4,1,'R',NULL,NULL),(4,2,'W','R','K1'),(4,3,'W',NULL,NULL),(4,4,'W',NULL,NULL),(4,5,'R',NULL,NULL),(4,6,'W',NULL,NULL),(4,7,'W',NULL,NULL),(4,8,'W',NULL,NULL),(4,9,'W',NULL,NULL),(4,10,'W',NULL,NULL),(4,11,'W',NULL,NULL),(4,12,'W',NULL,NULL),(4,13,'W',NULL,NULL),(5,1,'R',NULL,NULL),(5,2,'R',NULL,NULL),(5,3,'R',NULL,NULL),(5,4,'R',NULL,NULL),(5,5,'R',NULL,NULL),(5,6,'W',NULL,NULL),(5,7,'W',NULL,NULL),(5,8,'W',NULL,NULL),(5,9,'W',NULL,NULL),(5,10,'W',NULL,NULL),(5,11,'W',NULL,NULL),(5,12,'W',NULL,NULL),(5,13,'W',NULL,NULL),(6,1,'W',NULL,NULL),(6,2,'R',NULL,NULL),(6,3,'W',NULL,NULL),(6,4,'W',NULL,NULL),(6,5,'W',NULL,NULL),(6,6,'W',NULL,NULL),(6,7,'W',NULL,NULL),(6,8,'W',NULL,NULL),(6,9,'W',NULL,NULL),(6,10,'W',NULL,NULL),(6,11,'W',NULL,NULL),(6,12,'W',NULL,NULL),(6,13,'W',NULL,NULL),(7,1,'W',NULL,NULL),(7,2,'W',NULL,NULL),(7,3,'W',NULL,NULL),(7,4,'W',NULL,NULL),(7,5,'R',NULL,NULL),(7,6,'W',NULL,NULL),(7,7,'W',NULL,NULL),(7,8,'W',NULL,NULL),(7,9,'P',NULL,NULL),(7,10,'P',NULL,NULL),(7,11,'W',NULL,NULL),(7,12,'W',NULL,NULL),(7,13,'W',NULL,NULL),(8,1,'W',NULL,NULL),(8,2,'W',NULL,NULL),(8,3,'W',NULL,NULL),(8,4,'W',NULL,NULL),(8,5,'W',NULL,NULL),(8,6,'W',NULL,NULL),(8,7,'W',NULL,NULL),(8,8,'W',NULL,NULL),(8,9,'W',NULL,NULL),(8,10,'W',NULL,NULL),(8,11,'W',NULL,NULL),(8,12,'P',NULL,NULL),(8,13,'W',NULL,NULL),(9,1,'W',NULL,NULL),(9,2,'W',NULL,NULL),(9,3,'W',NULL,NULL),(9,4,'W',NULL,NULL),(9,5,'W',NULL,NULL),(9,6,'W',NULL,NULL),(9,7,'W',NULL,NULL),(9,8,'W',NULL,NULL),(9,9,'P',NULL,NULL),(9,10,'P',NULL,NULL),(9,11,'P',NULL,NULL),(9,12,'P',NULL,NULL),(9,13,'P',NULL,NULL),(10,1,'W',NULL,NULL),(10,2,'W',NULL,NULL),(10,3,'W',NULL,NULL),(10,4,'W',NULL,NULL),(10,5,'W',NULL,NULL),(10,6,'W',NULL,NULL),(10,7,'W',NULL,NULL),(10,8,'W',NULL,NULL),(10,9,'P',NULL,NULL),(10,10,'W',NULL,NULL),(10,11,'W',NULL,NULL),(10,12,'W','P','K1'),(10,13,'P',NULL,NULL),(11,1,'W',NULL,NULL),(11,2,'W',NULL,NULL),(11,3,'W',NULL,NULL),(11,4,'W',NULL,NULL),(11,5,'W',NULL,NULL),(11,6,'W',NULL,NULL),(11,7,'W',NULL,NULL),(11,8,'W',NULL,NULL),(11,9,'P',NULL,NULL),(11,10,'W',NULL,NULL),(11,11,'W',NULL,NULL),(11,12,'W','P','K2'),(11,13,'P',NULL,NULL),(12,1,'W',NULL,NULL),(12,2,'W',NULL,NULL),(12,3,'W',NULL,NULL),(12,4,'W',NULL,NULL),(12,5,'W',NULL,NULL),(12,6,'W',NULL,NULL),(12,7,'W',NULL,NULL),(12,8,'W',NULL,NULL),(12,9,'P',NULL,NULL),(12,10,'W',NULL,NULL),(12,11,'W',NULL,NULL),(12,12,'W',NULL,NULL),(12,13,'P',NULL,NULL),(13,1,'W',NULL,NULL),(13,2,'W',NULL,NULL),(13,3,'W',NULL,NULL),(13,4,'W',NULL,NULL),(13,5,'W',NULL,NULL),(13,6,'W',NULL,NULL),(13,7,'W',NULL,NULL),(13,8,'W',NULL,NULL),(13,9,'P',NULL,NULL),(13,10,'P',NULL,NULL),(13,11,'P',NULL,NULL),(13,12,'P',NULL,NULL),(13,13,'P',NULL,NULL);
 /*!40000 ALTER TABLE `board` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -59,25 +59,6 @@ CREATE TABLE `board_empty` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
--- Dumping structure for procedure iee2019067_schema.clean_board
-DROP PROCEDURE IF EXISTS `clean_board`;
-DELIMITER //
-CREATE PROCEDURE `clean_board`()
-BEGIN
-	REPLACE INTO board SELECT * FROM board_empty;
-	UPDATE show_winner SET status=0 WHERE id=1 or id=2; -- set status=0=no winner for players
-	DELETE FROM players WHERE piece_color='P';
-	INSERT INTO players (piece_color) VALUES ('P');
-	DELETE FROM players where piece_color='R';
-	INSERT INTO players (piece_color) VALUES ('R');
-        DELETE FROM position where id=1 or id=2;
-	INSERT INTO position VALUES (1, 4, 2), (2,10,12);
-	DELETE FROM game_status;
-	INSERT INTO game_status VALUES ("started","R",null,"2023-12-31 18:01:55");
-END//
-DELIMITER ;
-
-
 --
 -- Dumping data for table `board_empty`
 --
@@ -97,7 +78,7 @@ DROP TABLE IF EXISTS `game_status`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `game_status` (
   `status` enum('not active','initialized','started','ended','aborded') NOT NULL DEFAULT 'not active',
-  `p_turn` varchar(10) DEFAULT NULL,        -- enum('P','R')
+  `p_turn` varchar(10) DEFAULT NULL,
   `result` varchar(10) DEFAULT NULL,
   `last_change` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -109,31 +90,9 @@ CREATE TABLE `game_status` (
 
 LOCK TABLES `game_status` WRITE;
 /*!40000 ALTER TABLE `game_status` DISABLE KEYS */;
-INSERT INTO `game_status` VALUES ('started','R',null,'2023-12-31 18:01:55');
+INSERT INTO `game_status` VALUES ('not active',NULL,NULL,'2024-01-02 18:12:14');
 /*!40000 ALTER TABLE `game_status` ENABLE KEYS */;
 UNLOCK TABLES;
-
---Procedure for move
-DROP PROCEDURE IF EXISTS `move_piece`;
-DELIMITER //
-CREATE PROCEDURE `move_piece`(x1 tinyint,y1 tinyint,x2 tinyint,y2 tinyint,color_of_player char)
-BEGIN
-declare p, p_color varchar(2);
-select  piece into p  FROM `board` WHERE X=x1 AND Y=y1;
-select  piece_color into p_color  FROM `board` WHERE X=x1 AND Y=y1;
-if(color_of_player!=p_color) then 
-	update board
-	set piece=p, piece_color=p_color
-	where x=x2 and y=y2;
-	
-	UPDATE board
-	SET piece=null,piece_color=null
-	WHERE X=x1 AND Y=y1;
-	update game_status set p_turn=if(p_color='P','R','P');
-    end if;
-    END//
-DELIMITER ;
---end of procedure
 
 --
 -- Table structure for table `move_P`
@@ -207,21 +166,46 @@ CREATE TABLE `players` (
 
 LOCK TABLES `players` WRITE;
 /*!40000 ALTER TABLE `players` DISABLE KEYS */;
-INSERT INTO `players` VALUES ('mixalis','P','8fec6ad8366e07795edbd3736fe79d87','2023-12-31 16:36:57'),('petros','R','1f6ef8765c917c30f5bcbfed7442db69','2023-12-31 16:36:27');
+INSERT INTO `players` VALUES (NULL,'P',NULL,'2024-01-02 18:12:14'),(NULL,'R',NULL,'2024-01-02 18:12:14');
 /*!40000 ALTER TABLE `players` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+--
+-- Table structure for table `position`
+--
 
---table to identify the winner
+DROP TABLE IF EXISTS `position`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `position` (
+  `id` int(11) NOT NULL,
+  `x` tinyint(1) NOT NULL,
+  `y` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`,`x`,`y`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `position`
+--
+
+LOCK TABLES `position` WRITE;
+/*!40000 ALTER TABLE `position` DISABLE KEYS */;
+INSERT INTO `position` VALUES (1,4,2),(2,10,12);
+/*!40000 ALTER TABLE `position` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `show_winner`
+--
 
 DROP TABLE IF EXISTS `show_winner`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `show_winner` (
-  `id` int not null,
-  `winner` varchar(15) not null,
-  `status` int default 0,
+  `id` int(11) NOT NULL,
+  `winner` varchar(15) NOT NULL,
+  `status` int(11) DEFAULT 0,
   PRIMARY KEY (`id`,`winner`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -230,11 +214,56 @@ CREATE TABLE `show_winner` (
 -- Dumping data for table `show_winner`
 --
 
+
+
 LOCK TABLES `show_winner` WRITE;
 /*!40000 ALTER TABLE `show_winner` DISABLE KEYS */;
-INSERT INTO `show_winner` VALUES (1,"Red player", 0),(2,"Purple player", 0); //id=1 for red, id=2 for purple player. Status=0=no winner, status=1=winner
+INSERT INTO `show_winner` VALUES (1,'Red player',0),(2,'Purple player',0);
 /*!40000 ALTER TABLE `show_winner` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+-- Procedures
+
+-- Dumping structure for procedure iee2019067_schema.clean_board
+DROP PROCEDURE IF EXISTS `clean_board`; 
+DELIMITER //
+CREATE PROCEDURE `clean_board`()
+BEGIN
+	REPLACE INTO board SELECT * FROM board_empty;
+	UPDATE show_winner SET status=0 WHERE id=1 or id=2; -- set status=0=no winner for players
+	DELETE FROM players WHERE piece_color='P';
+	INSERT INTO players (piece_color) VALUES ('P');
+	DELETE FROM players where piece_color='R';
+	INSERT INTO players (piece_color) VALUES ('R');
+        DELETE FROM position where id=1 or id=2;
+	INSERT INTO position VALUES (1, 4, 2), (2,10,12);
+	DELETE FROM game_status;
+	INSERT INTO game_status VALUES ("started","R",null,"2023-12-31 18:01:55");
+END//
+DELIMITER ;
+
+
+-- Dumping structure for procedure iee2019067_schema.move_piece
+DROP PROCEDURE IF EXISTS `move_piece`;
+DELIMITER //
+CREATE PROCEDURE `move_piece`(x1 tinyint,y1 tinyint,x2 tinyint,y2 tinyint,color_of_player char)
+BEGIN
+declare p, p_color varchar(2);
+	select  piece into p  FROM `board` WHERE X=x1 AND Y=y1;
+	select  piece_color into p_color  FROM `board` WHERE X=x1 AND Y=y1;
+    if(color_of_player!=p_color) then 
+	update board
+	set piece=p, piece_color=p_color
+	where x=x2 and y=y2;
+	
+	UPDATE board
+	SET piece=null,piece_color=null
+	WHERE X=x1 AND Y=y1;
+	update game_status set p_turn=if(p_color='P','R','P');
+    end if;
+END//
+DELIMITER ;
 
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -245,5 +274,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-12-31 20:04:42
-
+-- Dump completed on 2024-01-02 20:15:13
